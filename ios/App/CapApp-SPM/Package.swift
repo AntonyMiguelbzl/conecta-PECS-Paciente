f5b7426a41e048caff655bbdc0b7fe254ee30cf7
AntonyMiguelbzl <antonymigueldapazfonseca@gmail.com>
2026-08-02 12:33:00 -0300

    Feat: Imagem Apk ; Ios e Android

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.2"),
-        .package(name: "CapacitorCommunityTextToSpeech", path: "../../../node_modules/@capacitor-community/text-to-speech")
+        .package(name: "CapacitorCommunityTextToSpeech", path: "..\..\..\node_modules\@capacitor-community\text-to-speech")
     ],
     targets: [
         .target(
